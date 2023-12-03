90a7b23d1ccc427864804a31be12b240be8ce825
kjbarrett <108308384+kjbarrett@users.noreply.github.com>
2023-12-02 16:01:51 -0800

    Restore images.js

--- KarelGotchiApp/assets/images/images.js
+++ KarelGotchiApp/assets/images/images.js
@@ -4,6 +4,10 @@
 // file/section within images file
 const images = {
   karel_basic: require("./karels/karel_basic.png"),
+  login_bg: require("./tamabg.png"),
+  karel_blue: require("./karels/karel_blue.png"),
+  karel_green: require("./karels/karel_green.png"),
+  karel_pink: require("./karels/karel_pink.png"),
 };
 
 export default images;
